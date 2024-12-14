5a93002176ca828ec0615669db16df412456867e
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2024-12-14 18:43:30 +0100

    fix again...

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -136,7 +136,7 @@ in
     bash = {
       # set alias for simple update
       shellAliases = {
-        update = "sudo nix flake update --commit-lock-file --flake /config/nix#beaststation";
+        update = "sudo nix flake update --commit-lock-file --flake /config/nix";
         upgrade = "sudo nixos-rebuild switch --flake /config/nix#beaststation";
       };
     };
