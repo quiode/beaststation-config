fd58c36754d4125ff060c7871fe230ddd5aa21e0
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2024-12-17 22:06:44 +0100

    Revert "install pci utils"
    This reverts commit 57a3d087f9ca7b80619ecfc79446955318863070.

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -53,7 +53,7 @@ in
 
   environment = {
     # packages
-    systemPackages = with pkgs; [ inputs.agenix.packages."${system}".default fastfetch onefetch btop sanoid pciutils ];
+    systemPackages = with pkgs; [ inputs.agenix.packages."${system}".default fastfetch onefetch btop sanoid ];
 
     # custom /etc stuff
     etc = {
