f6a8322dde579f87a99a7923d48110a82636ea7d
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2024-12-17 15:40:40 +0100

    add sanoid

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -53,7 +53,7 @@ in
 
   environment = {
     # packages
-    systemPackages = with pkgs; [ inputs.agenix.packages."${system}".default fastfetch onefetch btop ];
+    systemPackages = with pkgs; [ inputs.agenix.packages."${system}".default fastfetch onefetch btop sanoid ];
 
     # custom /etc stuff
     etc = {
