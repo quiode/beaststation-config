c197bc2248c8def4a549ca73ffef7245054f4329
beaststation <beaststation@dominik-schwaiger.ch>
2024-12-24 12:29:04 +0100

    update scripts

--- compose/down.sh
+++ compose/down.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Executes compose down for all
 for dir in */; do [ -f "$dir/docker-compose.yml" ] && (cd "$dir" && sudo docker compose down); done
--- compose/up.sh
+++ compose/up.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Executes compose up for all
 for dir in */; do [ -f "$dir/docker-compose.yml" ] && (cd "$dir" && sudo docker compose up -d); done
