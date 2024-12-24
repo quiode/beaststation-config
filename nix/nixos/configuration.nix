23c0903f09b74a3646826c654839d0be820c67bf
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2024-12-24 12:24:40 +0100

    install pv

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -53,7 +53,7 @@ in
 
   environment = {
     # packages
-    systemPackages = with pkgs; [ inputs.agenix.packages."${system}".default fastfetch onefetch btop sanoid dua gptfdisk htop ];
+    systemPackages = with pkgs; [ inputs.agenix.packages."${system}".default fastfetch onefetch btop sanoid dua gptfdisk htop pv ];
 
     # custom /etc stuff
     etc = {
