2da44530c06ad2db9c6f5e0fb6fc20aeebc1c153
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2024-12-24 15:50:36 +0100

    change stuff

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -342,6 +342,8 @@ in
       };
 
       loginShellInit = ''
+        # If not running interactively, don't do anything and return early
+        [[ $- == *i* ]] || return  
         fastfetch
       '';
     };
