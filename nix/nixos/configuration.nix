007e67483106879875638a5a46bbbf7af7fa4269
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-01-29 22:59:19 +0100

    fix udev file for wolf

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -63,24 +63,6 @@ in
           root: mail@dominik-schwaiger.ch
         '';
       };
-      "udev/rules.d/85-wolf-virtual-inputs.rules" = {
-        text = ''
-          # Allows Wolf to acces /dev/uinput
-          KERNEL=="uinput", SUBSYSTEM=="misc", MODE="0660", GROUP="input", OPTIONS+="static_node=uinput"
-
-          # Allows Wolf to access /dev/uhid
-          KERNEL=="uhid", TAG+="uaccess"
-
-          # Move virtual keyboard and mouse into a different seat
-          SUBSYSTEMS=="input", ATTRS{id/vendor}=="ab00", MODE="0660", GROUP="input", ENV{ID_SEAT}="seat9"
-
-          # Joypads
-          SUBSYSTEMS=="input", ATTRS{name}=="Wolf X-Box One (virtual) pad", MODE="0660", GROUP="input"
-          SUBSYSTEMS=="input", ATTRS{name}=="Wolf PS5 (virtual) pad", MODE="0660", GROUP="input"
-          SUBSYSTEMS=="input", ATTRS{name}=="Wolf gamepad (virtual) motion sensors", MODE="0660", GROUP="input"
-          SUBSYSTEMS=="input", ATTRS{name}=="Wolf Nintendo (virtual) pad", MODE="0660", GROUP="input"
-        '';
-      };
     };
   };
 
@@ -358,6 +340,31 @@ in
           recursive = true;
         };
       };
+    };
+
+    udev = {
+      packages = [
+        (pkgs.writeTextFile {
+          name = "wolf_udev";
+          text = ''
+            # Allows Wolf to acces /dev/uinput
+            KERNEL=="uinput", SUBSYSTEM=="misc", MODE="0660", GROUP="input", OPTIONS+="static_node=uinput"
+
+            # Allows Wolf to access /dev/uhid
+            KERNEL=="uhid", TAG+="uaccess"
+
+            # Move virtual keyboard and mouse into a different seat
+            SUBSYSTEMS=="input", ATTRS{id/vendor}=="ab00", MODE="0660", GROUP="input", ENV{ID_SEAT}="seat9"
+
+            # Joypads
+            SUBSYSTEMS=="input", ATTRS{name}=="Wolf X-Box One (virtual) pad", MODE="0660", GROUP="input"
+            SUBSYSTEMS=="input", ATTRS{name}=="Wolf PS5 (virtual) pad", MODE="0660", GROUP="input"
+            SUBSYSTEMS=="input", ATTRS{name}=="Wolf gamepad (virtual) motion sensors", MODE="0660", GROUP="input"
+            SUBSYSTEMS=="input", ATTRS{name}=="Wolf Nintendo (virtual) pad", MODE="0660", GROUP="input"
+          '';
+          destination = "/etc/udev/rules.d/85-wolf-virtual-inputs.rules";
+        })
+      ];
     };
   };
 
