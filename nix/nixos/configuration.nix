324db3a98c4f6aedc2a43101d60870922f73abfc
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-03-06 18:55:10 +0100

    keep 5 nh

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -425,7 +425,7 @@ in
     nh = {
       enable = true;
       clean.enable = true;
-      clean.extraArgs = "--keep-since 4d --keep 3";
+      clean.extraArgs = "--keep-since 4d --keep 5";
       flake = "/config/nix";
     };
   };
