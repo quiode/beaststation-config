0bc60783a377203308100f19df1b1a6f34087961
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-03-14 16:06:20 +0100

    add domina to secrets users

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,7 @@ let
   laptop-quio = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWkILtyyPWk4UYWJaZoI5UqGKo/qlaJG5h7zfS69+ie mail@dominik-schwaiger.ch";
   pc-quio = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINxfAbBPBerC/yizdTU3aWII4fsDWEwZBHmxMAhgNn7X quio@dominik-kaltbrunn-pc";
 
-  users = [ laptop-quio pc-quio ];
+  users = [ beaststation-domina laptop-quio pc-quio ];
 
   beaststation = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL/s6lYNuiiu10xgH91eUfyHMBumXa3wby0dP+PaVsaF root@beaststation";
 
