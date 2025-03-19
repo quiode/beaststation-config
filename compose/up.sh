120cfb1ca8c0e7993848f0bc914150a498d66f49
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-03-19 10:23:33 +0100

    no sudo

--- compose/up.sh
+++ compose/up.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Executes compose up for all
-for dir in */; do [ -f "$dir/docker-compose.yml" ] && (cd "$dir" && sudo docker compose up -d); done
+for dir in */; do [ -f "$dir/docker-compose.yml" ] && (cd "$dir" && docker compose up -d); done
