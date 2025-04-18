adaad75113fb08208b36bd4b66661b8a31b0c2c5
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-04-18 14:25:47 +0200

    change ssh key of pc at home

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -1,20 +1,19 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
-{ inputs
-, lib
-, config
-, pkgs
-, ...
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
 }:
 let
-  zfsCompatibleKernelPackages = lib.filterAttrs
-    (
-      name: kernelPackages:
-        (builtins.match "linux_[0-9]+_[0-9]+" name) != null
-        && (builtins.tryEval kernelPackages).success
-        && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
-    )
-    pkgs.linuxKernel.packages;
+  zfsCompatibleKernelPackages = lib.filterAttrs (
+    name: kernelPackages:
+    (builtins.match "linux_[0-9]+_[0-9]+" name) != null
+    && (builtins.tryEval kernelPackages).success
+    && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
+  ) pkgs.linuxKernel.packages;
   latestKernelPackage = lib.last (
     lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
       builtins.attrValues zfsCompatibleKernelPackages
@@ -42,7 +41,26 @@ in
 
   environment = {
     # packages
-    systemPackages = with pkgs; [ inputs.agenix.packages."${system}".default fastfetch onefetch btop sanoid dua gptfdisk htop pv zip unzip immich-cli speedtest-cli wget nvtopPackages.full zfs-prune-snapshots pciutils usbutils ];
+    systemPackages = with pkgs; [
+      inputs.agenix.packages."${system}".default
+      fastfetch
+      onefetch
+      btop
+      sanoid
+      dua
+      gptfdisk
+      htop
+      pv
+      zip
+      unzip
+      immich-cli
+      speedtest-cli
+      wget
+      nvtopPackages.full
+      zfs-prune-snapshots
+      pciutils
+      usbutils
+    ];
 
     # custom /etc stuff
     etc = {
@@ -89,7 +107,10 @@ in
     };
 
     zfs = {
-      extraPools = [ "virt" "hdd" ];
+      extraPools = [
+        "virt"
+        "hdd"
+      ];
     };
 
     # use the latest ZFS-compatible Kernel
@@ -113,7 +134,10 @@ in
         ssh = {
           enable = true;
           port = 2222;
-          authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWkILtyyPWk4UYWJaZoI5UqGKo/qlaJG5h7zfS69+ie mail@dominik-schwaiger.ch" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINxfAbBPBerC/yizdTU3aWII4fsDWEwZBHmxMAhgNn7X quio@dominik-kaltbrunn-pc" ];
+          authorizedKeys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWkILtyyPWk4UYWJaZoI5UqGKo/qlaJG5h7zfS69+ie mail@dominik-schwaiger.ch"
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINt4xvNKr0MsKk7qY9RJux9KGfUk2lCsnAeUO4NtJP8n quio@gaming-pc"
+          ];
           hostKeys = [
             /etc/secrets/initrd/ssh_host_rsa_key
             /etc/secrets/initrd/ssh_host_ed25519_key
@@ -134,7 +158,10 @@ in
   };
 
   # Enable the Flakes feature and the accompanying new nix command-line tool
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
   # enable docker
   virtualisation.docker = {
@@ -146,9 +173,18 @@ in
     };
     daemon.settings = {
       "default-address-pools" = [
-        { "base" = "172.29.0.0/16"; "size" = 24; }
-        { "base" = "172.30.0.0/16"; "size" = 24; }
-        { "base" = "172.31.0.0/16"; "size" = 24; }
+        {
+          "base" = "172.29.0.0/16";
+          "size" = 24;
+        }
+        {
+          "base" = "172.30.0.0/16";
+          "size" = 24;
+        }
+        {
+          "base" = "172.31.0.0/16";
+          "size" = 24;
+        }
       ];
     };
   };
@@ -162,8 +198,36 @@ in
     firewall = {
       enable = true;
 
-      allowedTCPPorts = [ 22 80 443 1194 2222 25565 25 143 389 465 587 636 993 ];
-      allowedUDPPorts = [ 22 80 443 1194 2222 25565 25 143 389 465 587 636 993 ];
+      allowedTCPPorts = [
+        22
+        80
+        443
+        1194
+        2222
+        25565
+        25
+        143
+        389
+        465
+        587
+        636
+        993
+      ];
+      allowedUDPPorts = [
+        22
+        80
+        443
+        1194
+        2222
+        25565
+        25
+        143
+        389
+        465
+        587
+        636
+        993
+      ];
     };
 
     # explicitly enable, needed for remote unlocking
@@ -183,10 +247,14 @@ in
         isNormalUser = true;
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWkILtyyPWk4UYWJaZoI5UqGKo/qlaJG5h7zfS69+ie mail@dominik-schwaiger.ch"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINxfAbBPBerC/yizdTU3aWII4fsDWEwZBHmxMAhgNn7X quio@dominik-kaltbrunn-pc"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINt4xvNKr0MsKk7qY9RJux9KGfUk2lCsnAeUO4NtJP8n quio@gaming-pc"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIINYO9+aRrPHh8WDkpcY0xSxJeFZg3nyjuhXkLOlBKIm"
         ];
-        extraGroups = [ "wheel" "docker" "video" ];
+        extraGroups = [
+          "wheel"
+          "docker"
+          "video"
+        ];
         group = "domina";
       };
 
@@ -194,14 +262,21 @@ in
         isNormalUser = true;
         extraGroups = [ ];
         group = "virt";
-        openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCvENmoNJtelL9NIB+c/KSanF5kajF+/SuS1FuA3n5a7l/XFEw0YHw8vRZqjsJYHHjCyKba0BZ0kVu96JtiLEcR6SE7rpESuJ8hVL0JS8sVCjp+jjpmMM60aOx7vXPqR6BiOnCK74EpucEXrotUl0KYD0TEwE9O1ArML9Pxz2VFQ/mFjmmGmOg46B3N302T6t4Ng+YzavUW9E5S1Lw5hxQPR2G4ujLSFeIwchTTqG7SpJfzmczJ8XPQ6SJ2fNTkXfTNBOBOe5d8g1XNrZz55njV9IWIIpnOPDpKfPYCuubiFgkQv89n7fpS/lLr1sNyYnGVowjamOI6GKzWmG+hqkpnLz0sx5clurr6nMYb6MlmFsBi9saBoWyVLAQOhrat882Sk3dd5NebEA5A53ctk4oVd92Wda6PYvaVsC5KC+fqztE1+Zvi8jJR21l3Yh/GmlRBplKp/WlUJ+MF9MD+/mOaU9Ca8EmibitIjEkv1/GnQHR2KsHaFBooF7pfBQh3mxhfE1RF6a1Y2zzSO5GDzdQlDCQmjvB8KT0vEScna5dhw9ys+sS8Q5pqtpouObUUL4DRFr27GUpzrdKNtrJ4bonWFhPeurQBsXEvpMT/Xl4rH0+2WNeiSmTv2+U8U1y7Ld+tQa296EfgZ+62CMQLV7JRuJYoPpkc6nNtCJ8FIJPrPw== joshua@board" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIffUHUiY5LIzpG5EfwT6mbulPlwejd7rSztrJmMohP1 backup@pool" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII9S1UMVtJr2CHLg2XuSMyl8m2F/ezKULST7kM7saY2r backup@home" "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC6sBthWuPYqE+Bds9QKfg9ka0L6CAXsf0NrEEIvbq6lJdUt34tp/IjPtJe0g0cydgXpdLk/hsOM1tn0h/hCXBy2YjUab/aJbRZPf+qXD9ZLg4o1w/nPc4a64BsLwYAdtiS9osq15zUpzibYRLaaKhLlF7v4LY0tiwU7hUO8kQs/jxDBDfvx4xTexCWHoWcBtX9xdQQSXqX3JKhVjQxMfsYxw1TJZ6LZduAIxo8kKKJ+WXl8JpORu+c98OQ++ed1UMIxaAeviOJ34+0EK/wDtakzqLUGXL7h5OZ07p7jBCiLo4WYhAMIwSS20tWP2J3aS02v0Xd+BAdsPW1iKIVlJGBK8XWDIlYn9+otJ+TR3DK9ROGFBKJlBCs7n7NBFchE/2NmI0O0DASm/jn6tjFumk7aOKvycCu102zTnxzpuO+1NlV/xmZS+DnQb/mQxZy+1CAKMmxBGoY+BiPHFpbpD7O59AY5fuOm2G/5J32CzKPXTuLdgjttzsPi/QkS7FFwh8= joshua@pad" ];
+        openssh.authorizedKeys.keys = [
+          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCvENmoNJtelL9NIB+c/KSanF5kajF+/SuS1FuA3n5a7l/XFEw0YHw8vRZqjsJYHHjCyKba0BZ0kVu96JtiLEcR6SE7rpESuJ8hVL0JS8sVCjp+jjpmMM60aOx7vXPqR6BiOnCK74EpucEXrotUl0KYD0TEwE9O1ArML9Pxz2VFQ/mFjmmGmOg46B3N302T6t4Ng+YzavUW9E5S1Lw5hxQPR2G4ujLSFeIwchTTqG7SpJfzmczJ8XPQ6SJ2fNTkXfTNBOBOe5d8g1XNrZz55njV9IWIIpnOPDpKfPYCuubiFgkQv89n7fpS/lLr1sNyYnGVowjamOI6GKzWmG+hqkpnLz0sx5clurr6nMYb6MlmFsBi9saBoWyVLAQOhrat882Sk3dd5NebEA5A53ctk4oVd92Wda6PYvaVsC5KC+fqztE1+Zvi8jJR21l3Yh/GmlRBplKp/WlUJ+MF9MD+/mOaU9Ca8EmibitIjEkv1/GnQHR2KsHaFBooF7pfBQh3mxhfE1RF6a1Y2zzSO5GDzdQlDCQmjvB8KT0vEScna5dhw9ys+sS8Q5pqtpouObUUL4DRFr27GUpzrdKNtrJ4bonWFhPeurQBsXEvpMT/Xl4rH0+2WNeiSmTv2+U8U1y7Ld+tQa296EfgZ+62CMQLV7JRuJYoPpkc6nNtCJ8FIJPrPw== joshua@board"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIffUHUiY5LIzpG5EfwT6mbulPlwejd7rSztrJmMohP1 backup@pool"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII9S1UMVtJr2CHLg2XuSMyl8m2F/ezKULST7kM7saY2r backup@home"
+          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC6sBthWuPYqE+Bds9QKfg9ka0L6CAXsf0NrEEIvbq6lJdUt34tp/IjPtJe0g0cydgXpdLk/hsOM1tn0h/hCXBy2YjUab/aJbRZPf+qXD9ZLg4o1w/nPc4a64BsLwYAdtiS9osq15zUpzibYRLaaKhLlF7v4LY0tiwU7hUO8kQs/jxDBDfvx4xTexCWHoWcBtX9xdQQSXqX3JKhVjQxMfsYxw1TJZ6LZduAIxo8kKKJ+WXl8JpORu+c98OQ++ed1UMIxaAeviOJ34+0EK/wDtakzqLUGXL7h5OZ07p7jBCiLo4WYhAMIwSS20tWP2J3aS02v0Xd+BAdsPW1iKIVlJGBK8XWDIlYn9+otJ+TR3DK9ROGFBKJlBCs7n7NBFchE/2NmI0O0DASm/jn6tjFumk7aOKvycCu102zTnxzpuO+1NlV/xmZS+DnQb/mQxZy+1CAKMmxBGoY+BiPHFpbpD7O59AY5fuOm2G/5J32CzKPXTuLdgjttzsPi/QkS7FFwh8= joshua@pad"
+        ];
       };
 
       vali = {
         isNormalUser = true;
         extraGroups = [ ];
         group = "vali";
-        openssh.authorizedKeys.keys = [ "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAAWEDj/Yib6Mqs016jx7rtecWpytwfVl28eoHtPYCM9TVLq81VIHJSN37lbkc/JjiXCdIJy2Ta3A3CVV5k3Z37NbgAu23oKA2OcHQNaRTLtqWlcBf9fk9suOkP1A3NzAqzivFpBnZm3ytaXwU8LBJqxOtNqZcFVruO6fZxJtg2uE34mAw==" ];
+        openssh.authorizedKeys.keys = [
+          "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAAWEDj/Yib6Mqs016jx7rtecWpytwfVl28eoHtPYCM9TVLq81VIHJSN37lbkc/JjiXCdIJy2Ta3A3CVV5k3Z37NbgAu23oKA2OcHQNaRTLtqWlcBf9fk9suOkP1A3NzAqzivFpBnZm3ytaXwU8LBJqxOtNqZcFVruO6fZxJtg2uE34mAw=="
+        ];
       };
     };
   };
@@ -334,14 +409,20 @@ in
       # use custom ssh key
       sshKey = /etc/secrets/syncoid/id_ed25519;
 
-      commonArgs = [ "--delete-target-snapshots" "--no-sync-snap" ];
+      commonArgs = [
+        "--delete-target-snapshots"
+        "--no-sync-snap"
+      ];
 
       commands = {
         "hdd/enc/critical" = {
           source = "hdd/enc/critical";
           target = "domina@yniederer.ch:backup/hdd";
           sendOptions = "w";
-          extraArgs = [ "--sshport" "2222" ];
+          extraArgs = [
+            "--sshport"
+            "2222"
+          ];
           recursive = true;
         };
 
@@ -349,7 +430,10 @@ in
           source = "rpool/ssd/critical";
           target = "domina@yniederer.ch:backup/ssd";
           sendOptions = "w";
-          extraArgs = [ "--sshport" "2222" ];
+          extraArgs = [
+            "--sshport"
+            "2222"
+          ];
           recursive = true;
         };
       };
@@ -424,7 +508,8 @@ in
     ssh = {
       startAgent = true;
       knownHosts = {
-        "[yniederer.ch]:2222".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOxx2JxRobdvqPUIDgl0xFHoF0UVjNGNGmQzqg0xr210";
+        "[yniederer.ch]:2222".publicKey =
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOxx2JxRobdvqPUIDgl0xFHoF0UVjNGNGmQzqg0xr210";
       };
     };
 
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,13 @@
 let
   beaststation-domina = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAION61ovdLLEFvBSEnM0KHflttJFbOe3KDAwIShZM7uTd domina@beaststation";
   laptop-quio = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWkILtyyPWk4UYWJaZoI5UqGKo/qlaJG5h7zfS69+ie mail@dominik-schwaiger.ch";
-  pc-quio = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINxfAbBPBerC/yizdTU3aWII4fsDWEwZBHmxMAhgNn7X quio@dominik-kaltbrunn-pc";
+  pc-quio = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINt4xvNKr0MsKk7qY9RJux9KGfUk2lCsnAeUO4NtJP8n quio@gaming-pc";
 
-  users = [ beaststation-domina laptop-quio pc-quio ];
+  users = [
+    beaststation-domina
+    laptop-quio
+    pc-quio
+  ];
 
   beaststation = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL/s6lYNuiiu10xgH91eUfyHMBumXa3wby0dP+PaVsaF root@beaststation";
 
