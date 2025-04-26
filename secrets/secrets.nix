1e151e120ee603a4ef7471b2644fd2edc7bef737
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-04-26 21:59:22 +0200

    ssh: update laptop agenix key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   beaststation-domina = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAION61ovdLLEFvBSEnM0KHflttJFbOe3KDAwIShZM7uTd domina@beaststation";
-  laptop-quio = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWkILtyyPWk4UYWJaZoI5UqGKo/qlaJG5h7zfS69+ie mail@dominik-schwaiger.ch";
+  laptop-quio = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGpYUYVbj55SB3zK+W+oUq2AdO3sS27ZeTtGVYpdq3Dd quio@laptop";
   pc-quio = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINt4xvNKr0MsKk7qY9RJux9KGfUk2lCsnAeUO4NtJP8n quio@gaming-pc";
 
   users = [
@@ -11,10 +11,9 @@ let
 
   beaststation = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL/s6lYNuiiu10xgH91eUfyHMBumXa3wby0dP+PaVsaF root@beaststation";
 
-  systems = [ beaststation ];
+  systems = [beaststation];
 
   all = users ++ systems;
-in
-{
+in {
   "beaststation_mail_password.age".publicKeys = all;
 }
