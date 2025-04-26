1e7c4098ff6e8792839287bd36e854e074be7823
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-04-26 21:56:58 +0200

    ssh: add smartphone biometric ssh key

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -138,6 +138,7 @@ in {
           authorizedKeys = [
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINt4xvNKr0MsKk7qY9RJux9KGfUk2lCsnAeUO4NtJP8n quio@gaming-pc"
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGpYUYVbj55SB3zK+W+oUq2AdO3sS27ZeTtGVYpdq3Dd quio@laptop"
+            "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBL/+jpXlMFM8n4BqvAiPKshuQlHnEkwowrYjq9EExLzrCGpY8D47lAQYh/YkiwILYPHEiznfP7bLCvVifwp1QKI= quio@OnePlusNord"
           ];
           hostKeys = [
             /etc/secrets/initrd/ssh_host_rsa_key
@@ -225,6 +226,7 @@ in {
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINt4xvNKr0MsKk7qY9RJux9KGfUk2lCsnAeUO4NtJP8n quio@gaming-pc"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGpYUYVbj55SB3zK+W+oUq2AdO3sS27ZeTtGVYpdq3Dd quio@laptop"
+          "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBL/+jpXlMFM8n4BqvAiPKshuQlHnEkwowrYjq9EExLzrCGpY8D47lAQYh/YkiwILYPHEiznfP7bLCvVifwp1QKI= quio@OnePlusNord"
         ];
         extraGroups = [
           "wheel"
